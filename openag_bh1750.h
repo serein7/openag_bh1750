dbc91d24bd3f824152e1eda228d64cae042dde1a
Siarhei Yaromin <siarhei_yaromin@epam.com>
2016-09-04 09:40:24 +0300

    draft implementation

---
+++ openag_bh1750.h
@@ -0,0 +1,50 @@
+/**
+ *  \file openag_bh1750.h
+ *  \brief Light intensity sensor based on BH1750 chip.
+ */
+/***************************************************
+  Based on sample code from http://blog.simtronyx.de/en/measurement-of-illuminance-with-a-bh1750fvi-breakout-board-gy-30-and-an-arduino-uno/
+
+  Modified for OpenAg
+ ****************************************************/
+#ifndef OPENAG_BH1750
+#define OPENAG_BH1750
+
+#if defined(ARDUINO) && (ARDUINO >= 100)
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#if defined(__AVR__)
+  #include <util/delay.h>
+#endif
+
+#include <Wire.h>
+#include <openag_module.h>
+#include <std_msgs/Float32.h>
+
+/**
+ * \brief Air temperature and air humidity sensor.
+ */
+class Bh2315 : public Module {
+
+  public:
+    void begin();
+    void update();
+    bool get_light_illuminance(std_msgs::Float32 &msg);
+
+  private:
+    // Private variables
+    float _light_illuminance;
+    bool _send_light_illuminance;
+    uint32_t _time_of_last_reading;
+    const static uint32_t _min_update_interval = 2000;
+    const static int _i2c_address = 0x23;
+    const static int _read_register = 0x03;
+
+    // Private methods
+    void readData();
+};
+
+#endif
